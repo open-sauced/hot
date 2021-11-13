fa2ba0cc8c4b080f8ec549ead181249740bf26f2
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-11-12 16:03:00 -0800

    fixes ogs error

--- src/components/Recos.jsx
+++ src/components/Recos.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Recos({data}) {
-  return <img src={data.image.url} alt={data.title} />;
+  return <img src={data.ogImage.url} alt={data.title} />;
 }

b123e0d1fff1da8d912db8ced19aa55cc6d8a740
Dane Miller <khadane.miller@gmail.com>
2021-12-21 21:34:38 -0500

    style: adddress linting issues

--- src/lib/getAvatar.test.js
+++ src/lib/getAvatar.test.js
@@ -1,14 +1,14 @@
-import getAvatar from "./getAvatar";
+import getAvatar from './getAvatar';
 
-describe("Test: getAvatar()", () => {
-  test("Passing nulfrost should return return the correct URL", () => {
-    let expected = "https://github.com/nulfrost.png?size=460";
-    let actual = getAvatar("nulfrost");
+describe('Test: getAvatar()', () => {
+  test('Passing nulfrost should return return the correct URL', () => {
+    const expected = 'https://github.com/nulfrost.png?size=460';
+    const actual = getAvatar('nulfrost');
     expect(expected).toBe(actual);
   });
-  test("Passing nothing should return the github avatar", () => {
-    let expected = "https://github.com/github.png?size=460";
-    let actual = getAvatar();
+  test('Passing nothing should return the github avatar', () => {
+    const expected = 'https://github.com/github.png?size=460';
+    const actual = getAvatar();
     expect(expected).toBe(actual);
   });
 });

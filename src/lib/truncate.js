276beb91a9a0eb96a10a69ebbe4a1687cb875a7c
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-12-26 08:50:48 -0500

    lint truncate

--- src/lib/truncate.js
+++ src/lib/truncate.js
@@ -1,7 +1,6 @@
-export default function truncate(str, length = 100, ending = "...") {
+export default function truncate(str, length = 100, ending = '...') {
   if (str.length > length) {
     return str.substring(0, length - ending.length) + ending;
-  } else {
-    return str;
   }
+  return str;
 }
--- src/lib/truncate.test.js
+++ src/lib/truncate.test.js
@@ -1,15 +1,15 @@
-import truncate from "./truncate";
+import truncate from './truncate';
 
-describe("Test: truncate()", () => {
-  test("string should truncate with length", () => {
-    const string = "test/testtesttest";
+describe('Test: truncate()', () => {
+  test('string should truncate with length', () => {
+    const string = 'test/testtesttest';
     const result = truncate(string, 12);
-    expect(result).toBe("test/test...");
+    expect(result).toBe('test/test...');
   });
 
   test("'string should truncate with ending", () => {
-    const string = "test/testtesttest";
-    const result = truncate(string, 12, "!!!");
-    expect(result).toBe("test/test!!!");
+    const string = 'test/testtesttest';
+    const result = truncate(string, 12, '!!!');
+    expect(result).toBe('test/test!!!');
   });
 });

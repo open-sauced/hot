b7e5aefcbafa7a570968c3d796fab74610139bac
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-12-27 12:00:54 +0100

    fix: Improved the card UI look

--- src/components/PostGrid.jsx
+++ src/components/PostGrid.jsx
@@ -5,7 +5,6 @@ import cover2 from "./placeholders/cover2.jpg";
 import humanizeNumber from "../lib/humanizeNumber";
 import truncate from "../lib/truncate";
 import getAvatar from "../lib/getAvatar";
-
 function PostGrid({ data }) {
   const [repoOwner, repoName] = data.repo_name.split("/");
 
@@ -14,75 +13,88 @@ function PostGrid({ data }) {
     option === "issues" ? window.open(`${repoLink}/issues`) : window.open(repoLink);
   };
 
-  return (
-    <div className=" bg-offWhite rounded-xl p-6 font-roboto cursor-pointer ">
-      {/* Avator Container */}
-      <div className=" w-full flex  mb-3 ">
-        <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
-          <img
-            className="object-cover"
-            src={getAvatar(data?.contributors[0])}
-            alt="Avatar 01"
-            width={500}
-            height={500}
-          />
-        </div>
-        <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
-          <img
-            className="object-cover"
-            src={getAvatar(data?.contributors[1])}
-            alt="Avatar 02"
-            width={500}
-            height={500}
-          />
-        </div>
-      </div>
-      {/* Title */}
-      <div className=" text-grey text-md font-medium mb-1 h-14 overflow-hidden cursor-pointer" onClick={handleClick}>
-        <h1>{data.repo_name} </h1>
-      </div>
-      {/* Description */}
-      <div className=" text-lightGrey text-sm mb-2 ">
-        <h3> {truncate(data.description)} </h3>{" "}
-      </div>
-      {/* Cover photo */}
+  const handleRedirect = (contributor) => {
+    // window.open(`https://github.com/${contributor}`);
+  };
 
-      <div className="w-full bg-transparent h-28 overflow-hidden rounded-md mb-2">
-        <img
-          className="object-cover"
-          src={`https://opengraph.githubassets.com/1/${data.repo_name}`}
-          alt="Avatar 02"
-          width={500}
-          height={500}
-        />
+  return (
+    <div className=" bg-offWhite rounded-xl p-6 font-roboto cursor-pointer flex flex-col justify-between ">
+      <div className="flex flex-col">
+        {/* Avator Container */}
+        <div className=" w-full flex ">
+          <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
+            <img
+              className="object-cover"
+              src={getAvatar(data?.contributors[0])}
+              alt="Avatar 01"
+              width={500}
+              height={500}
+              onClick={handleRedirect(data?.contributors[0])}
+            />
+          </div>
+          <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
+            <img
+              className="object-cover"
+              src={getAvatar(data?.contributors[1])}
+              alt="Avatar 02"
+              width={500}
+              height={500}
+              onClick={handleRedirect(data?.contributors[1])}
+            />
+          </div>
+        </div>
+        {/* Title */}
+        <div
+          className=" text-grey text-sm font-medium mb-1 h-14 overflow-hidden cursor-pointer flex items-center"
+          onClick={handleClick}
+        >
+          <h1>{data.repo_name} </h1>
+        </div>
+        {/* Description */}
+        <div className=" text-lightGrey text-sm mb-2 ">
+          <h3> {truncate(data.description)} </h3>{" "}
+        </div>
+        {/* Cover photo */}
+        <div className="w-full bg-transparent h-28 overflow-hidden rounded-md mb-2 flex justify-center">
+          <img
+            className="object-contain h-full"
+            src={`https://opengraph.githubassets.com/1/${data.repo_name}`}
+            alt={`${data.repo_name}`}
+          />
+        </div>
       </div>
+
+      {/* Cover photo and action button container */}
+      {/* <div className="flex flex-col border-2 w-full "> */}
+
       {/* Action Button Container */}
-      <div className=" flex justify-between w-full ">
+      <div className=" flex justify-between w-full">
         {/* Upvote */}
-        <div className=" flex justify-center items-center text-lg text-grey hover:text-saucyRed cursor-pointer transition-all duration-200  ">
-          <i className="fas fa-arrow-alt-circle-up mr-2 "></i>
+        <div className=" flex justify-center items-center text-base text-grey hover:text-saucyRed cursor-pointer transition-all duration-200  space-x-1">
+          <i className="fas fa-arrow-alt-circle-up  "></i>
           <p className="font-bold">5</p>
         </div>
 
         {/* Issues */}
         <div
-          className=" flex justify-center items-center text-lg text-grey hover:text-saucyRed cursor-pointer transition-all duration-200  "
+          className=" flex justify-center items-center text-base text-grey hover:text-saucyRed cursor-pointer transition-all duration-200 space-x-1 "
           onClick={() => handleClick("issues")}
         >
-          <i className="fas fa-comment-dots mr-2 "></i>
+          <i className="fas fa-comment-dots "></i>
 
           {data.issues && <p className="font-bold">{humanizeNumber(data.issues)}</p>}
         </div>
 
         {/* Stars */}
         <div
-          className=" flex justify-center items-center text-lg text-grey hover:text-saucyRed cursor-pointer transition-all duration-200 "
+          className=" flex justify-center items-center text-base text-grey hover:text-saucyRed cursor-pointer transition-all duration-200 space-x-1"
           onClick={handleClick}
         >
-          <i className="fas fa-star mr-2 "></i>
+          <i className="fas fa-star "></i>
           {data.total_stars && <p className="font-bold">{humanizeNumber(data.stars)}</p>}
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 }

fcee7903333f17716e31a62c7e17c407995c0938
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-12-28 23:01:24 +0100

    fix: merge conflict resolved and npm ci to restore package.json and npm-shrinkwrap.json

--- src/components/PostGrid.jsx
+++ src/components/PostGrid.jsx
@@ -19,28 +19,39 @@ function PostGrid({ data }) {
   };
 
   return (
-    <div className=" bg-offWhite rounded-xl p-6 font-roboto cursor-pointer ">
+    <div className=" bg-offWhite rounded-xl pt-6 px-4 pb-2 font-roboto cursor-pointer ">
       {/* Avator Container */}
-      <div className=" w-full flex  mb-3 ">
-        <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
-          <img
-            className="object-cover"
-            src={getAvatar(data?.contributors[0])}
-            alt="Avatar 01"
-            width={500}
-            height={500}
-            onClick={() => handleRedirect(data?.contributors[0])}
-          />
+      <div className=" w-full flex justify-between items-center mb-3 ">
+        <div className="flex w-full">
+          <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
+            <img
+              className="object-cover"
+              src={getAvatar(data?.contributors[0])}
+              alt="Avatar 01"
+              width={500}
+              height={500}
+              onClick={() => handleRedirect(data?.contributors[0])}
+            />
+          </div>
+          <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
+            <img
+              className="object-cover"
+              src={getAvatar(data?.contributors[1])}
+              alt="Avatar 02"
+              width={500}
+              height={500}
+              onClick={() => handleRedirect(data?.contributors[1])}
+            />
+          </div>
         </div>
-        <div className="bg-blue-400 w-10 h-10 overflow-hidden  rounded-full mr-3 ">
-          <img
-            className="object-cover"
-            src={getAvatar(data?.contributors[1])}
-            alt="Avatar 02"
-            width={500}
-            height={500}
-            onClick={() => handleRedirect(data?.contributors[1])}
-          />
+        <div className="flex">
+          <div
+            className=" flex justify-center items-center text-base space-x-1 text-grey 
+        hover:text-saucyRed cursor-pointer transition-all duration-200  "
+          >
+            <i className="fas fa-arrow-alt-circle-up "></i>
+            <p className="font-bold">5</p>
+          </div>
         </div>
       </div>
       {/* Title */}
@@ -49,7 +60,7 @@ function PostGrid({ data }) {
       </div>
       {/* Description */}
       <div className=" text-lightGrey text-sm mb-2 ">
-        <h3> {truncate(data.description)} </h3>{" "}
+        <h3> {truncate(data.description)} </h3>
       </div>
       {/* Cover photo */}
       <div className="w-full bg-transparent h-32 overflow-hidden rounded-md mb-2 flex justify-center">
@@ -62,29 +73,31 @@ function PostGrid({ data }) {
       {/* Action Button Container */}
       <div className=" flex justify-between w-full ">
         {/* Upvote */}
-        {/* <div className=" flex justify-center items-center text-xltext-grey hover:text-saucyRed cursor-pointer transition-all duration-200  ">
+        {/* <div className=" flex justify-center items-center text-xltext-grey 
+        hover:text-saucyRed cursor-pointer transition-all duration-200  ">
           <i className="fas fa-arrow-alt-circle-up mr-2 "></i>
           <p className="font-bold">5</p>
         </div> */}
         {/* Issues */}
-        <div
-          className=" flex justify-center items-center text-xl text-grey hover:text-saucyRed cursor-pointer transition-all duration-200  "
+        {/* <div
+          className=" flex justify-center items-center text-xl text-grey 
+          hover:text-saucyRed cursor-pointer transition-all duration-200  "
           onClick={() => handleClick("issues")}
         >
           <i className="fas fa-comment-dots mr-2 "></i>
 
           {data.issues && <p className="font-bold">{humanizeNumber(data.issues)}</p>}
-        </div>{" "}
-        */}
+        </div> */}
+
         {/* Stars */}
-        <div
-          className=" flex justify-center items-center text-xltext-grey hover:text-saucyRed cursor-pointer transition-all duration-200 "
+        {/* <div
+          className=" flex justify-center items-center text-xltext-grey
+          hover:text-saucyRed cursor-pointer transition-all duration-200 "
           onClick={handleClick}
         >
           <i className="fas fa-star mr-2 "></i>
           {data.total_stars && <p className="font-bold">{humanizeNumber(data.stars)}</p>}
-        </div>{" "}
-        */}
+        </div> */}
       </div>
     </div>
   );

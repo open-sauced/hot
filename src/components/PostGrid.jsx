a9167e088452063597b8d93fbf550ab9aa2f3079
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-12-30 21:48:25 +0100

    fix: removed title, description and adjusted the grid

--- src/components/PostGrid.jsx
+++ src/components/PostGrid.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import av1 from "./placeholders/av01.jpg";
-import av2 from "./placeholders/av02.jpg";
-import cover2 from "./placeholders/cover2.jpg";
-import humanizeNumber from "../lib/humanizeNumber";
-import truncate from "../lib/truncate";
+
 import getAvatar from "../lib/getAvatar";
 
 function PostGrid({ data }) {
@@ -20,8 +16,9 @@ function PostGrid({ data }) {
 
   return (
     <div className=" bg-offWhite rounded-xl pt-6 px-4 pb-2 font-roboto cursor-pointer ">
-      {/* Avator Container */}
+      {/* Avatar & upvote container */}
       <div className=" w-full flex justify-between items-center mb-3">
+        {/* Avator Container */}
         <div className="flex w-full">
           <div className="bg-blue-400 w-10 h-10 overflow-hidden rounded-full mr-3">
             <img
@@ -44,6 +41,7 @@ function PostGrid({ data }) {
             />
           </div>
         </div>
+        {/* Upvote container */}
         <div className="flex">
           <div
             className=" flex justify-center items-center text-base space-x-1 text-grey 
@@ -54,14 +52,6 @@ function PostGrid({ data }) {
           </div>
         </div>
       </div>
-      {/* Title */}
-      <div className=" text-grey text-sm font-medium mb-1 h-14 overflow-hidden cursor-pointer" onClick={handleClick}>
-        <h1>{data.repo_name} </h1>
-      </div>
-      {/* Description */}
-      <div className=" text-lightGrey text-sm mb-2 ">
-        <h3> {truncate(data.description)} </h3>
-      </div>
       {/* Cover photo */}
       <div className="w-full bg-transparent h-32 overflow-hidden rounded-md mb-2 flex justify-center">
         <img
@@ -69,35 +59,6 @@ function PostGrid({ data }) {
           src={`https://opengraph.githubassets.com/1/${data.repo_name}`}
           alt="Avatar 02"
         />
-      </div>
-      {/* Action Button Container */}
-      <div className=" flex justify-between w-full ">
-        {/* Upvote */}
-        {/* <div className=" flex justify-center items-center text-xltext-grey 
-        hover:text-saucyRed cursor-pointer transition-all duration-200  ">
-          <i className="fas fa-arrow-alt-circle-up mr-2 "></i>
-          <p className="font-bold">5</p>
-        </div> */}
-        {/* Issues */}
-        {/* <div
-          className=" flex justify-center items-center text-xl text-grey 
-          hover:text-saucyRed cursor-pointer transition-all duration-200  "
-          onClick={() => handleClick("issues")}
-        >
-          <i className="fas fa-comment-dots mr-2 "></i>
-
-          {data.issues && <p className="font-bold">{humanizeNumber(data.issues)}</p>}
-        </div> */}
-
-        {/* Stars */}
-        {/* <div
-          className=" flex justify-center items-center text-xltext-grey
-          hover:text-saucyRed cursor-pointer transition-all duration-200 "
-          onClick={handleClick}
-        >
-          <i className="fas fa-star mr-2 "></i>
-          {data.total_stars && <p className="font-bold">{humanizeNumber(data.stars)}</p>}
-        </div> */}
       </div>
     </div>
   );
--- src/components/PostsWrap.jsx
+++ src/components/PostsWrap.jsx
@@ -13,7 +13,7 @@ const PostsWrap = () => {
       <LayoutToggle gridState={isGrid} setGridState={setIsGrid} />
       <div className="bg-darkestGrey py-6 w-full min-h-screen">
         {isGrid ? (
-          <div className=" container grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 ">
+          <div className=" container grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
             {postData.map((item, i) => (
               <PostGrid data={item} key={i} />
             ))}

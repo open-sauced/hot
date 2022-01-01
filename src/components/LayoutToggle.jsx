3169538cf563d3357bde03e56937fb5770ddab95
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-01-01 19:40:20 +0100

    feat: implemented filter of secondary navbar

--- src/components/LayoutToggle.jsx
+++ src/components/LayoutToggle.jsx
@@ -4,7 +4,7 @@ function LayoutToggle({ gridState, setGridState }) {
   const position = !!gridState ? "left-0" : "right-0";
 
   return (
-    <div className=" bg-darkestGrey md:pt-8 pb-5 md:pb-14">
+    <div className=" bg-darkestGrey md:pt-4 pb-5">
       <div className="container flex justify-center ">
         <div className="bg-saucyRed w-20 h-8 rounded-xl flex items-center cursor-pointer relative  text-grey text-md ">
           <div className={"absolute bottom-0.1 w-10 h-10 rounded-xl bg-gray-100   " + position}></div>
---
+++ src/components/SecondaryNav.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const SecondaryNav = ({ activeLink, setActiveLink }) => {
+  // console.log(activeLink);
+  const handleChange = (e) => {
+    e.preventDefault();
+    const linkName = e.target.getAttribute("data-name");
+    setActiveLink(linkName);
+  };
+  return (
+    <div>
+      <div className="bg-darkestGrey py-14 md:py-16">
+        <nav className="container">
+          <ul className="flex flex-col space-y-2 sm:flex-row text-xl font-righteous text-accent font-bold justify-center items-center cursor-pointer">
+            <li
+              data-name="popular"
+              onClick={(e) => handleChange(e)}
+              className={`${
+                activeLink === "popular" ? "bg-cheesyYellow rounded-xl text-grey " : " "
+              } hover:text-saucyRed transition-all duration-300 mr-3 p-2 sm:mr-11`}
+            >
+              Popular
+            </li>
+            <li
+              data-name="upvoted"
+              onClick={(e) => handleChange(e)}
+              // className="hover:text-saucyRed transition-all duration-300 mr-3 sm:mr-11"
+              className={`${
+                activeLink === "upvoted" ? "bg-cheesyYellow rounded-xl text-grey " : " "
+              } hover:text-saucyRed transition-all duration-300 mr-3 p-2 sm:mr-11`}
+            >
+              Upvoted
+            </li>
+            <li
+              data-name="discussed"
+              onClick={(e) => handleChange(e)}
+              // className="hover:text-saucyRed transition-all duration-300 mr-3 sm:mr-11"
+              className={`${
+                activeLink === "discussed" ? "bg-cheesyYellow rounded-xl text-grey " : " "
+              } hover:text-saucyRed transition-all duration-300 mr-3 p-2 sm:mr-11`}
+            >
+              Discussed
+            </li>
+            <li
+              data-name="recent"
+              onClick={(e) => handleChange(e)}
+              // className="hover:text-saucyRed  transition-all duration-300 mr-3 sm:mr-11"
+              className={`${
+                activeLink === "recent" ? "bg-cheesyYellow rounded-xl text-grey " : " "
+              } hover:text-saucyRed transition-all duration-300 mr-3 p-2 sm:mr-11`}
+            >
+              Recent
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default SecondaryNav;

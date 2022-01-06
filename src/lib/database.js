6093e83fc478325791ccf0bddab98f430f67e9d0
brian <ilikerobot@gmail.com>
2022-01-05 19:31:31 -0800

    fix format

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // probably should move these to an env.
-const supabase = createClient('https://ibcwmlhcimymasokhgvn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTkzMDc3OCwiZXhwIjoxOTQ1NTA2Nzc4fQ.zcdbd7kDhk7iNSMo8SjsTaXi0wlLNNQcSZkzZ84NUDg')
+const supabase = createClient('https://ibcwmlhcimymasokhgvn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTkzMDc3OCwiZXhwIjoxOTQ1NTA2Nzc4fQ.zcdbd7kDhk7iNSMo8SjsTaXi0wlLNNQcSZkzZ84NUDg');
 
 export async function fetchVotesByRepo(repoName) {
   const { data: recommendations, error } = await supabase
@@ -17,7 +17,7 @@ export async function fetchVotesByRepo(repoName) {
 export async function updateVotesByRepo(repoName, votes) {
   const { data: recommendations, error } = await supabase
     .from('recommendations')
-    .update({'votes': votes + 1})
+    .update({ votes: votes + 1 })
     .eq('repo_name', repoName);
 
   console.error(error);

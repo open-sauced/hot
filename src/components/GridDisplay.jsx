b3bdc0e3a173704fc848087fee297fda2ef6b235
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2022-01-11 00:45:09 -0500

    remotved unused variable

--- src/components/GridDisplay.jsx
+++ src/components/GridDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PostGrid from './PostGrid.jsx';
 
-const GridDisplay = ({ fetchedData, activeLink }) => (
+const GridDisplay = ({ fetchedData }) => (
     <div>
       <div className=" container grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-screen-xl mx-auto">
         {fetchedData.map((item, i) => <PostGrid data={item} key={`${item.repo_name}_${i}`} />)}
--- src/components/ListDisplay.jsx
+++ src/components/ListDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PostList from './PostList.jsx';
 
-const ListDisplay = ({ fetchedData, activeLink }) => (
+const ListDisplay = ({ fetchedData }) => (
     <div>
       <div className=" container space-y-3 max-w-screen-xl mx-auto ">
         {fetchedData.map((item, i) => <PostList data={item} key={`${item.repo_name}_${i}`} />)}

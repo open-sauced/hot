6e8f71916d797cb94283989935850b930bcc736f
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-01-13 22:31:51 +0100

    feat: PostGrid social image to redirect to repo

--- src/components/PostsWrap.jsx
+++ src/components/PostsWrap.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import LayoutToggle from './LayoutToggle.jsx';
+import React, { useState, useEffect } from "react";
+import LayoutToggle from "./LayoutToggle.jsx";
 
-import Modal from './Modal.jsx';
-import SecondaryNav from './SecondaryNav.jsx';
-import GridDisplay from './GridDisplay.jsx';
-import ListDisplay from './ListDisplay.jsx';
-import { fetchRecommendations } from '../lib/database';
+import Modal from "./Modal.jsx";
+import SecondaryNav from "./SecondaryNav.jsx";
+import GridDisplay from "./GridDisplay.jsx";
+import ListDisplay from "./ListDisplay.jsx";
+import { fetchRecommendations } from "../lib/database";
 
 const activeLinkColumns = {
-  popular: { orderBy: 'total_stars' },
-  upvoted: { orderBy: 'votes' },
-  discussed: { orderBy: 'issues' },
-  recent: { orderBy: 'avg_recency_score' },
+  popular: { orderBy: "total_stars" },
+  upvoted: { orderBy: "votes" },
+  discussed: { orderBy: "issues" },
+  recent: { orderBy: "avg_recency_score" },
 };
 
 const PostsWrap = () => {
   const [isGrid, setIsGrid] = useState(true);
-  const [activeLink, setActiveLink] = useState('popular');
+  const [activeLink, setActiveLink] = useState("popular");
   const [fetchedData, setFetchedData] = useState([]);
 
   useEffect(() => {
@@ -32,11 +32,7 @@ const PostsWrap = () => {
       <SecondaryNav activeLink={activeLink} setActiveLink={setActiveLink} />
       <LayoutToggle gridState={isGrid} setGridState={setIsGrid} />
       <div className="bg-darkestGrey py-6 w-full min-h-screen">
-        {isGrid ? (
-          <GridDisplay fetchedData={fetchedData} activeLink={activeLink} />
-        ) : (
-          <ListDisplay fetchedData={fetchedData} activeLink={activeLink} />
-        )}
+        {isGrid ? <GridDisplay fetchedData={fetchedData} /> : <ListDisplay fetchedData={fetchedData} />}
       </div>
     </>
   );

b9902b9de3e36b028bb76ead58d294e362b51914
brian <ilikerobot@gmail.com>
2022-01-16 01:13:19 -0800

    fix error

--- src/components/GridDisplay.jsx
+++ src/components/GridDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PostGrid from './PostGrid.jsx';
 
-const GridDisplay = ({ fetchedData }) => (
+const GridDisplay = ({ fetchedData, user }) => (
   <div>
     <div className=" container grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-screen-xl mx-auto">
       {fetchedData.map((item, i) => (
@@ -14,7 +14,6 @@ const GridDisplay = ({ fetchedData }) => (
 
 GridDisplay.propTypes = {
   fetchedData: PropTypes.array.isRequired,
-  activeLink: PropTypes.string.isRequired,
 };
 
 export default GridDisplay;

8aa263e25b0737c74e6253e0a2b1b4217f41e29d
yhoungdev <yhoungdev@gmail.com>
2022-01-18 23:20:50 +0100

    feat: Service worker added

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,12 @@
 module.exports = {
-	globDirectory: 'public/',
-	globPatterns: [
-		'**/*.{svg,txt,css}'
-	],
-	swDest: 'public/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: 'public/',
+  globPatterns: [
+    '**/*.{svg,txt,css}',
+  ],
+  swDest: 'public/sw.js',
+  ignoreURLParametersMatching: [
+    /^utm_/,
+    /^fbclid$/,
+  ],
+
 };

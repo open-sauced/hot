392dd7274019b1cb799a41f16a5c5441964727d6
Rajat <rs20021023@gmail.com>
2022-01-29 16:08:45 +0530

    feat:Linked to user profile in the user dropdown

--- src/components/PrimaryNav.jsx
+++ src/components/PrimaryNav.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Menu } from '@headlessui/react';
-import logo from './logo.svg';
-import useSupabaseAuth from '../hooks/useSupabaseAuth';
-import { version } from '../../package.json';
+import React from "react";
+import { Menu } from "@headlessui/react";
+import logo from "./logo.svg";
+import useSupabaseAuth from "../hooks/useSupabaseAuth";
+import { version } from "../../package.json";
 
 const PrimaryNav = () => {
   const { signIn, signOut, user } = useSupabaseAuth();
@@ -20,60 +20,64 @@ const PrimaryNav = () => {
         </a>
       </div>
 
-      {!user && <div className="items-center">
-        <div
-          className="cursor-pointer"
-          onClick={async () => {
-            await signIn({ provider: 'github' });
-          }}
-        >
-          Login
-        </div>
-      </div>}
-     {user && (
-        <Menu as="div" className="relative inline-block text-left">
-        <Menu.Button>
+      {!user && (
         <div className="items-center">
           <div
-            className="rounded-full w-10 h-10 overflow-hidden ring-2 ring-red-800"
+            className="cursor-pointer"
+            onClick={async () => {
+              await signIn({ provider: "github" });
+            }}
           >
-            {user && <img className="object-cover w-[500] h-[500]" src={user.user_metadata.avatar_url} alt={`${user.user_metadata.user_name}-avatar`}/>}
+            Login
           </div>
         </div>
-        </Menu.Button>
-        <Menu.Items className="absolute right-0 w-56 origin-top-right rounded-md shadow-lg shadow-gray-700/80 border-gray-700 border-2 focus:outline-none px-1 py-1 bg-darkestGrey text-sm font-semibold">
-          <Menu.Item>
-            {({ active }) => (
-              <span
-                className={`${active && 'bg-gray-700'} block px-4 py-2 rounded-md text-gray-200`}
-              >
-               {user.user_metadata.user_name}
-              </span>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <span
-                className={`${active && 'bg-gray-700'} block px-4 py-2 rounded-md text-gray-200`}
-              >
-                v{version}
-              </span>
-            )}
-          </Menu.Item>
-          <Menu.Item onClick={async () => {
-            await signOut();
-          }}>
-            {({ active }) => (
-              <span
-                className={`${active && 'bg-gray-700'} block px-4 py-2 rounded-md text-gray-200 cursor-pointer`}
-              >
-                Logout
-              </span>
-            )}
-          </Menu.Item>
-        </Menu.Items>
-      </Menu>
-     )}
+      )}
+      {user && (
+        <Menu as="div" className="relative inline-block text-left">
+          <Menu.Button>
+            <div className="items-center">
+              <div className="rounded-full w-10 h-10 overflow-hidden ring-2 ring-red-800">
+                {user && (
+                  <img
+                    className="object-cover w-[500] h-[500]"
+                    src={user.user_metadata.avatar_url}
+                    alt={`${user.user_metadata.user_name}-avatar`}
+                  />
+                )}
+              </div>
+            </div>
+          </Menu.Button>
+          <Menu.Items className="absolute right-0 w-56 origin-top-right rounded-md shadow-lg shadow-gray-700/80 border-gray-700 border-2 focus:outline-none px-1 py-1 bg-darkestGrey text-sm font-semibold">
+            <Menu.Item>
+              {({ active }) => (
+                <a href={`https://github.com/${user.user_metadata.user_name}`}>
+                  <span className={`${active && "bg-gray-700"} block px-4 py-2 rounded-md text-gray-200`}>
+                    {user.user_metadata.user_name}
+                  </span>
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span className={`${active && "bg-gray-700"} block px-4 py-2 rounded-md text-gray-200`}>
+                  v{version}
+                </span>
+              )}
+            </Menu.Item>
+            <Menu.Item
+              onClick={async () => {
+                await signOut();
+              }}
+            >
+              {({ active }) => (
+                <span className={`${active && "bg-gray-700"} block px-4 py-2 rounded-md text-gray-200 cursor-pointer`}>
+                  Logout
+                </span>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </Menu>
+      )}
     </nav>
   );
 };

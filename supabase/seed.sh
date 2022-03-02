f91ca82a20240b255cb71869854d21985ef49427
TED Vortex <ted.vortex@gmail.com>
2022-03-02 02:25:02 +0200

    fix: correct local seeding and auth

--- supabase/seed.sh
+++ supabase/seed.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
+REPO="https://github.com/open-sauced/actions/raw/main/hot-supabase/seed"
+PGDB="postgresql://postgres:postgres@localhost:5432/postgres"
+tables=(recommendations,stars,user_stars,users,votes)
+export PGPASSWORD=postgres
+
 apt-get update
 apt-get install -y curl
 
 mkdir -p /supabase
 cd supabase
 
-curl -sLO https://github.com/open-sauced/actions/raw/main/hot-supabase/seed/recommendations.csv
-curl -sLO https://github.com/open-sauced/actions/raw/main/hot-supabase/seed/stars.csv
-curl -sLO https://github.com/open-sauced/actions/raw/main/hot-supabase/seed/user_stars.csv
-curl -sLO https://github.com/open-sauced/actions/raw/main/hot-supabase/seed/users.csv
-curl -sLO https://github.com/open-sauced/actions/raw/main/hot-supabase/seed/votes.csv
+curl -sLO $REPO/recommendations.csv
+curl -sLO $REPO/stars.csv
+curl -sLO $REPO/user_stars.csv
+curl -sLO $REPO/users.csv
+curl -sLO $REPO/votes.csv
 
-export PGPASSWORD=postgres
+for i in "${tables[@]}"
+do :
+  psql $PGDB -c "TRUNCATE public.${i};"
+done
 
-psql 'postgresql://postgres:postgres@localhost:5432/postgres' -c "\COPY recommendations(id, repo_name, stars, issues, description, contributors, total_stars, avg_recency_score, votes) FROM 'recommendations.csv' DELIMITER ',' CSV HEADER;"
-psql 'postgresql://postgres:postgres@localhost:5432/postgres' -c "\COPY stars(forks_count, stargazers_count, open_issues_count, full_name, id) FROM 'stars.csv' DELIMITER ',' CSV HEADER;"
-psql 'postgresql://postgres:postgres@localhost:5432/postgres' -c "\COPY user_stars(star_id, id, user_id, repo_name, recency_score, description, issues, stars, contributors) FROM 'user_stars.csv' DELIMITER ',' CSV HEADER;"
-psql 'postgresql://postgres:postgres@localhost:5432/postgres' -c "\COPY users(id, login, private, open_issues, stars_data) FROM 'users.csv' DELIMITER ',' CSV HEADER;"
-psql 'postgresql://postgres:postgres@localhost:5432/postgres' -c "\COPY votes(id, created_at, github_user_id, repo_name, code) FROM 'votes.csv' DELIMITER ',' CSV HEADER;"
+psql $PGDB -c "\COPY recommendations(id, stars, issues, total_stars, votes, avg_recency_score, repo_name, description, contributors, created_at) FROM 'recommendations.csv' DELIMITER ',' CSV HEADER;"
+psql $PGDB -c "\COPY stars(id, stargazers_count, open_issues_count, forks_count, full_name, created_at) FROM 'stars.csv' DELIMITER ',' CSV HEADER;"
+psql $PGDB -c "\COPY user_stars(id, user_id, star_id, recency_score, issues, stars, repo_name, description, contributors, created_at) FROM 'user_stars.csv' DELIMITER ',' CSV HEADER;"
+psql $PGDB -c "\COPY users(id, open_issues, private, stars_data, login, created_at) FROM 'users.csv' DELIMITER ',' CSV HEADER;"
+psql $PGDB -c "\COPY votes(id, github_user_id, repo_name, code, created_at) FROM 'votes.csv' DELIMITER ',' CSV HEADER;"
 
 ls -lahH .

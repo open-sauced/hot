0415039d9054ac5427872e747ea076c803d4c2b2
Dane Miller <khadane.miller@gmail.com>
2022-03-08 18:43:44 -0500

    fix: add a min-height to the load more button to match the card height (#145)
    fixes #128

--- src/components/GridDisplay.tsx
+++ src/components/GridDisplay.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import PostGrid from './PostGrid';
-import {User} from "@supabase/supabase-js";
+import React from "react";
+import PostGrid from "./PostGrid";
+import { User } from "@supabase/supabase-js";
 
 export declare interface GridDisplayProps {
   activeLink: string | null;
@@ -10,19 +10,20 @@ export declare interface GridDisplayProps {
   user: User | null;
 }
 
-const GridDisplay = ({activeLink, limit, handleLoadingMore, fetchedData, user}: GridDisplayProps): JSX.Element => (
+const GridDisplay = ({ activeLink, limit, handleLoadingMore, fetchedData, user }: GridDisplayProps): JSX.Element => (
   <div>
-    <div className="container grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-screen-xl mx-auto">
+    <div className="container grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-screen-xl mx-auto auto-rows-fr">
       {fetchedData.map((item, i) => (
-        <PostGrid user={user} data={item} key={`${item.repo_name}_${i}`}/>))}
-      {fetchedData.length > 0
-        && activeLink !== 'myVotes'
-        && limit <= 100
-        && <button onClick={() => handleLoadingMore()}
-          className="bg-grey hover:bg-lightGrey text-white font-bold py-2 px-4 rounded-xl">
+        <PostGrid user={user} data={item} key={`${item.repo_name}_${i}`} />
+      ))}
+      {fetchedData.length > 0 && activeLink !== "myVotes" && limit <= 100 && (
+        <button
+          onClick={() => handleLoadingMore()}
+          className="bg-grey hover:bg-lightGrey text-white font-bold py-2 px-4 rounded-xl"
+        >
           Load More
         </button>
-      }
+      )}
     </div>
   </div>
 );

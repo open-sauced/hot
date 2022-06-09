8314e2784fff3d2862a3506021d65f69f5e33db0
Anthony Beckford <abeckford532@gmail.com>
2022-06-09 17:57:51 -0400

    Open source part 1

--- src/components/PostGrid.tsx
+++ src/components/PostGrid.tsx
@@ -6,6 +6,10 @@ import useSupabaseAuth from '../hooks/useSupabaseAuth';
 import { User } from "@supabase/supabase-js";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 import { capturePostHogAnayltics } from '../lib/analytics';
+import * as dayjs from 'dayjs' ;
+
+// import { relativeTime } from "dayjs/plugin/relativeTime";
+// dayjs.extend(relativeTime)
 
 export declare interface PostGridProps {
   data: DbRecomendation;
@@ -41,7 +45,8 @@ const PostGrid = ({ data, user }: PostGridProps): JSX.Element => {
           {data?.contributions[1] &&
             <Avatar
               contributor={data.contributions[1]?.contributor}
-              lastPr={data.contributions[1]?.last_merged_at}/>}
+              lastPr={dayjs().from now
+              //(dayjs(data.contributions[0]?.last_merged_at), true)}/>}
         </div>
 
         <div className="flex">

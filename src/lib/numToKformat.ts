985816a0757a4ab36da68bccc95c0af453df719f
shamimbinnnur <sbnsbn00@gmail.com>
2022-07-13 22:57:03 +0600

    typed numToKFormat

--- src/lib/numToKformat.ts
+++ src/lib/numToKformat.ts
@@ -1,10 +1,13 @@
-const numToKFormat = (num: number)=> {
+const numToKFormat = (num: number): string => {
     if(num > 999 && num < 1000000){
         return (num/1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million 
     }else if(num > 1000000){
         return (num/1000000).toFixed(1) + 'M'; // convert to M for number from > 1 million 
     }else if(num < 900){
-        return num; // if value < 1000, nothing to do
+        return num.toFixed(); // if value < 1000, nothing to do
+    }
+    else{
+        return num.toFixed();
     }
 }
 

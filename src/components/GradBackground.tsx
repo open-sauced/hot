85794e6106b919884ee30e57f82bf5efa12ac679
shamimbinnnur <sbnsbn00@gmail.com>
2022-07-19 14:05:38 +0600

    added alt text

--- src/components/GradBackground.tsx
+++ src/components/GradBackground.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'react'
 import gradBG from '../../public/gradBG.svg'
 
-
 const GradBackground: FC = ({ children }) => {
   return (
     <div className="h-fit relative">
       <div className="absolute w-[100vw] left-[-3px] z-[-1]">
-        <img className='mx-auto ' src={gradBG} alt="" />
+        <img className='mx-auto ' src={gradBG} alt="Gradient background" />
       </div>
 
       {children}

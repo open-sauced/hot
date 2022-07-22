7bee11758b943cbd0698b3d304b34c0ad2a763c5
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-07-23 00:12:40 +0100

    fix: destructured recommendation of hot repos

--- src/components/HotRepositories.tsx
+++ src/components/HotRepositories.tsx
@@ -5,6 +5,7 @@ import { AiOutlineStar } from "react-icons/ai";
 import { BiGitPullRequest } from "react-icons/bi";
 import { RiCheckboxCircleFill } from "react-icons/ri";
 import hotIcon from "../assets/hotIcon.png";
+
 const hotRepo = [
   {
     id: 357728069,
@@ -68,9 +69,9 @@ const HotRepositories = () => {
   const handleVoted = (id: number) => {
     const votedIdx = hotRepos.findIndex((obj) => obj.id == id);
     hotRepos[votedIdx].upvoted = !hotRepos[votedIdx].upvoted;
-
     setHotRepos([...hotRepos]);
   };
+
   return (
     <div className="flex flex-col px-4 max-w-screen-xl mx-auto">
       <div className="flex space-x-3 items-center">
@@ -78,28 +79,28 @@ const HotRepositories = () => {
         <h1 className="text-white font-bold text-2xl">Hot Repositories</h1>
       </div>
       <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full my-5">
-        {hotRepos.map((repo) => (
-          <div key={repo.id} className="p-4 border rounded-lg bg-white w-full space-y-1 relative">
+        {hotRepos.map(({ id, orgImg, organization, name, description, upvoted, issues, stars, PR, img }) => (
+          <div key={id} className="p-4 border rounded-lg bg-white w-full space-y-1 relative">
             {/* header & upvote button */}
             <div className="flex justify-between w-full">
               <div className="flex space-x-1 items-center">
-                <img src={repo.orgImg} alt="Hot Repo Icon" className="h-4 w-4 rounded-md overflow-hidden" />
-                <span className="text-xs text-gray-400">{repo.organization}</span>
+                <img src={orgImg} alt="Hot Repo Icon" className="h-4 w-4 rounded-md overflow-hidden" />
+                <span className="text-xs text-gray-400">{organization}</span>
               </div>
               <button
                 className={`px-2 py-0.5 border rounded-lg flex justify-center items-center space-x-1 text-[10px] transition-all duration-200 ${
-                  repo.upvoted ? "text-saucyRed border-saucyRed " : "text-grey border-gray-500 "
+                  upvoted ? "text-saucyRed border-saucyRed " : "text-grey border-gray-500 "
                 }`}
-                onClick={() => handleVoted(repo.id)}
+                onClick={() => handleVoted(id)}
               >
-                <span className="">{repo.upvoted ? "voted" : "upvote"}</span>
-                {repo.upvoted ? <RiCheckboxCircleFill className="" /> : <FaArrowAltCircleUp className="" />}
+                <span className="">{upvoted ? "voted" : "upvote"}</span>
+                {upvoted ? <RiCheckboxCircleFill className="" /> : <FaArrowAltCircleUp className="" />}
               </button>
             </div>
             {/* repo name & description */}
             <div className="flex flex-col pb-10">
-              <h1 className="text-xl font-semibold">{repo.name}</h1>
-              <p className="text-gray-500 text-xs w-5/6">{repo.description}</p>
+              <h1 className="text-xl font-semibold">{name}</h1>
+              <p className="text-gray-500 text-xs w-5/6">{description}</p>
             </div>
             {/* issues || star || PRs || Avatar */}
             <div className="flex items-center justify-between absolute bottom-3 inset-x-0 px-4">
@@ -107,22 +108,22 @@ const HotRepositories = () => {
               <div className="flex space-x-3 text-xs">
                 <div className="flex space-x-1 justify-center items-center">
                   <VscIssues />
-                  <span>{repo.issues}</span>
+                  <span>{issues}</span>
                 </div>
 
                 <div className="flex space-x-1 justify-center items-center">
                   <AiOutlineStar />
-                  <span>{repo.stars}</span>
+                  <span>{stars}</span>
                 </div>
 
                 <div className="flex space-x-1 justify-center items-center">
                   <BiGitPullRequest />
-                  <span>{repo.PR}</span>
+                  <span>{PR}</span>
                 </div>
               </div>
               {/* Avatars */}
               <div className="-space-x-2 flex hover:space-x-0 transition-all duration-300 ">
-                {repo.img.map((avatarImg) => (
+                {img.map((avatarImg) => (
                   <img
                     key={avatarImg}
                     src={avatarImg}

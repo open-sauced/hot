0d381804bcf047dcdc5f11074ef29de85c4dd637
shamimbinnnur <sbnsbn00@gmail.com>
2022-07-27 16:25:40 +0600

    css reset

--- src/components/ListDisplay.tsx
+++ src/components/ListDisplay.tsx
@@ -11,20 +11,27 @@ export declare interface ListDisplayProps {
 }
 
 const ListDisplay = ({activeLink, limit, handleLoadingMore, fetchedData, user}: ListDisplayProps): JSX.Element => (
-  <div>
-    <div className="container space-y-3 max-w-screen-xl mx-auto">
-      {fetchedData.map((item, i) =>
-        <PostList user={user} data={item} key={`${item.full_name}_${i}`}/>)}
-      {fetchedData.length > 0
+  <div className="">
+    {
+      fetchedData.map((item, i) =>
+        <PostList
+        user={user}
+        data={item}
+        key={`${item.full_name}_${i}`}
+        />
+      )
+    }
+    {
+      fetchedData.length > 0
         && activeLink !== 'myVotes'
         && limit <= 100
-        && <button
+        &&
+        <button
           onClick={() => handleLoadingMore()}
           className="bg-grey hover:bg-lightGrey text-white font-bold py-2 px-4 rounded-xl">
           Load More
         </button>
-      }
-    </div>
+    }
   </div>
 );
 

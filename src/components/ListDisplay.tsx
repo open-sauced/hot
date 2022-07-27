4971472dc23a083f0f970fa03c6af14717a56ac6
shamimbinnnur <sbnsbn00@gmail.com>
2022-07-27 17:36:01 +0600

    ui refresh

--- src/components/ListDisplay.tsx
+++ src/components/ListDisplay.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostList from './PostList';
 import {User} from "@supabase/supabase-js";
+import popularIcon from "../assets/popularIcon.svg"
 
 export declare interface ListDisplayProps {
   activeLink: string | null;
@@ -11,16 +12,23 @@ export declare interface ListDisplayProps {
 }
 
 const ListDisplay = ({activeLink, limit, handleLoadingMore, fetchedData, user}: ListDisplayProps): JSX.Element => (
-  <div className="">
-    {
-      fetchedData.map((item, i) =>
-        <PostList
-        user={user}
-        data={item}
-        key={`${item.full_name}_${i}`}
-        />
-      )
-    }
+  <div className="mx-auto max-w-7xl px-[16px] mt-[40px]">
+    <div className='flex flex-col gap-y-[20px]'>
+      <div className='flex items-center gap-x-[10px]'>
+        <img className='w-[26px] h-[26px]' src={popularIcon} alt="Popular" />
+        <h1 className='text-[24px]'>Popular repositories</h1>
+      </div>
+      {
+        fetchedData.map((item, i) =>
+          <PostList
+          user={user}
+          data={item}
+          key={`${item.full_name}_${i}`}
+          />
+        )
+      }
+    </div>
+
     {
       fetchedData.length > 0
         && activeLink !== 'myVotes'

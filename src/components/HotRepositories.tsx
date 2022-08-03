eebc38c0373a689fd209cec2ef87a1e91e06f943
Shardul Pathak <48183855+shapaaa@users.noreply.github.com>
2022-08-04 03:18:32 +0530

    style: Add styles missing for featured cards (#274)

--- src/components/HotRepositories.tsx
+++ src/components/HotRepositories.tsx
@@ -74,21 +74,21 @@ const HotRepositories = ({ user }: HotReposProps): JSX.Element => {
       <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full my-5">
         {hotRepos.map(
           ({ id, full_name, name, description, issues, stars, contributions }) => (
-            <div key={id} className="p-4 border rounded-lg bg-white w-full space-y-1 relative">
+            <div key={id} className="p-4 border rounded-2xl bg-white w-full space-y-1 relative">
               {/* header & upvote button */}
               <div className="flex justify-between w-full">
                 <div className="flex space-x-1 items-center">
                   <img src={getAvatarLink(full_name.replace(`/${name}`, ''))} alt="Hot Repo Icon" className="h-4 w-4 rounded-md overflow-hidden" />
-                  <span className="text-xs text-gray-400">{full_name.replace(`/${name}`, '')}</span>
+                  <span className="text-sm font-medium text-lightSlate11">{full_name.replace(`/${name}`, '')}</span>
                 </div>
                 <button
-                  className={`px-2 py-0.5 border rounded-lg flex justify-center items-center space-x-1 text-xs transition-all duration-200 ${
-                    checkVoted(id) ? "text-saucyRed border-saucyRed " : "text-grey border-gray-500 "
+                  className={`px-2 py-0.5 border  rounded-lg flex justify-center items-center space-x-1 text-xs transition-all duration-200 ${checkVoted(id)?'bg-lightOrange01':'bg-lightSlate01'} ${
+                    checkVoted(id) ? "text-saucyRed border-saucyRed " : "text-lightSlate11 border-lightSlate06 "
                   }`}
                   onClick={() => (user_id ? handleVoteUpdateByRepo(0, id) : signIn({ provider: "github" }))}
                 >
                   <span className="">{checkVoted(id) ? "voted" : "upvote"}</span>
-                  {checkVoted(id) ? <RiCheckboxCircleFill className="" /> : <FaArrowAltCircleUp className="" />}
+                  {checkVoted(id) ? <RiCheckboxCircleFill className="" /> : <FaArrowAltCircleUp className="fill-lightSlate09" />}
                 </button>
               </div>
               {/* repo name & description */}
@@ -101,25 +101,25 @@ const HotRepositories = ({ user }: HotReposProps): JSX.Element => {
                 >
                   {name}
                 </a>
-                <p className="text-gray-500 text-xs w-5/6">{description}</p>
+                <p className="text-gray-500 font-medium text-xs w-5/6">{description}</p>
               </div>
               {/* issues || star || PRs || Avatar */}
               <div className="flex items-center justify-between absolute bottom-3 inset-x-0 px-4">
                 {/* issues || star || PRs*/}
-                <div className="flex space-x-3 text-xs">
-                  <div className="flex space-x-1 justify-center items-center">
-                    <VscIssues />
-                    <span>{humanizeNumber(issues)}</span>
+                <div className="flex  space-x-3 text-xs">
+                  <div className="flex  text-sm space-x-1 justify-center items-center">
+                    <VscIssues size={16} className="fill-lightSlate10"/>
+                    <span className="text-lightSlate11">{humanizeNumber(issues)}</span>
                   </div>
 
-                  <div className="flex space-x-1 justify-center items-center">
-                    <AiOutlineStar />
-                    <span>{humanizeNumber(stars)}</span>
+                  <div className="flex  text-sm space-x-1 justify-center items-center">
+                    <AiOutlineStar size={16} className="fill-lightSlate10" />
+                    <span className="text-lightSlate11">{humanizeNumber(stars)}</span>
                   </div>
 
-                  <div className="flex space-x-1 justify-center items-center">
-                    <BiGitPullRequest />
-                    <span>0</span>
+                  <div className="flex  text-sm space-x-1 justify-center items-center">
+                    <BiGitPullRequest size={16} className="fill-lightSlate10" />
+                    <span className="text-lightSlate11">0</span>
                   </div>
                 </div>
                 {/* Avatars */}

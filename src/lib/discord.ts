83e2176130b09c863c1c5d821aec39a006528990
takanome_dev <takanomedev221@gmail.com>
2022-08-16 22:11:08 +0000

    refactor: uncomment discordjs request

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,21 +1,20 @@
 const sendMessage = (user: string, repoUrl: string) => {
-  console.log({ user, repoUrl });
+  const request = (new XMLHttpRequest);
 
-  // const request = (new XMLHttpRequest);
+  request.open(
+    "POST",
+    "https://discord.com/api/webhooks/998629882764476448/ItO3_kUsGTKdDgqc6sTrraOmJFhIVTQmSbygV0z-dk-IjUXRB6H7MN5zakOB6ili7YTd",
+  );
 
-  // request.open("POST", "https://discord.com/api/webhooks/998629882764476448/ItO3_kUsGTKdDgqc6sTrraOmJFhIVTQmSbygV0z-dk-IjUXRB6H7MN5zakOB6ili7YTd");
+  request.setRequestHeader("Content-type", "application/json");
 
-  // request.setRequestHeader("Content-type", "application/json");
+  const params = {
+    username: "Hot Sauced!",
+    avatar_url: "",
+    content: `${user} has made a new hot repo submission: [${repoUrl}](https://github.com/${repoUrl})`,
+  };
 
-  /*
-   * const params = {
-   *   username: "Hot Sauced!",
-   *   avatar_url: "",
-   *   content: `${user} has made a new hot repo submission: [${repoUrl}](https://github.com/${repoUrl})`,
-   * };
-   */
-
-  // request.send(JSON.stringify(params));
+  request.send(JSON.stringify(params));
 };
 
 export { sendMessage };

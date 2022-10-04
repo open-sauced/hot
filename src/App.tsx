1549690ebce23b2a57d8b98033c5d603c0d35b28
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2022-10-04 16:12:03 -0700

    feat: adds localstorageprovider
    fix #335

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,42 @@ console.log(
 const App = (): JSX.Element => {
   initiatePostHog();
 
+  const localStorageProvider = () => {
+    if (!import.meta.env.SSR) {
+      // console.log("You are on the browser");
+
+      /*
+       * console.log(localStorage)
+       * when initializing, we restore the data from `localStorage` into a map.
+       */
+
+      const appCache = localStorage.getItem("app-cache") ?? "[]";
+      const JSONdata = (JSON.parse(appCache));
+      const map = new Map(JSONdata);
+
+      // before unloading the app, we write back all the data into `localStorage`.
+      window.addEventListener("beforeunload", () => {
+        const appCache = JSON.stringify(Array.from(map.entries()));
+
+        localStorage.setItem("app-cache", appCache);
+      });
+
+      // we still use the map for write & read for performance.
+      return map;
+    }
+    // console.log("You are on the server");
+
+    // 👉️ can't use localStorage
+
+    return (new Map);
+  };
+
   return (
     <SWRConfig
       value={{
         revalidateOnFocus: false,
         fetcher: apiFetcher,
+        provider: localStorageProvider,
       }}
     >
       <Toaster position="top-right" />

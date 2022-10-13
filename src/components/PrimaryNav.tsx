079938efce50157fff032fb2d429fd612f51f970
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-10-13 14:06:11 -0400

    passing the token in header and successfully getting 200

--- src/components/PrimaryNav.tsx
+++ src/components/PrimaryNav.tsx
@@ -38,12 +38,10 @@ const PrimaryNav = (): JSX.Element => {
 
   useEffect(() => {
     const fetchAuthSession = async () => {
-      try {
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/session`, { headers: { accept: "application/json" } });
-
-        console.log("response: ", res);
-      } catch (err) {
-        console.log("error: ", err);
+      if (currentUser?.access_token) {
+        await fetch(`${import.meta.env.VITE_API_URL}/auth/session`, { headers: { accept: "application/json", Authorization: `Bearer ${currentUser.access_token}` } })
+          .then(res => console.log("response: ", res))
+          .catch(err => console.log("error: ", err));
       }
     };
 

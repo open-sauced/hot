9a157b37b6505338328de27bd3afc5321f718774
takanome_dev <takanomedev221@gmail.com>
2022-10-18 20:52:10 +0000

    refactor: update loading skeletons

--- src/components/ListRepositories.tsx
+++ src/components/ListRepositories.tsx
@@ -15,22 +15,12 @@ const ListRepositories = ({
   limit,
   handleLoadingMore,
   fetchedData,
-}: ListRepositoriesProps): JSX.Element => (
-  <div className="mx-auto max-w-7xl px-4 mt-10">
-    <div className="flex flex-col gap-y-5">
-      <div className="flex items-center gap-x-2.5">
-        <BsFillCalendar2Fill className="w-8 h-8 text-white" />
-
-        {activeLink && (
-          <h1 className="text-2xl text-white font-semibold">
-            {`${camelCaseToTitleCase(activeLink)} Repositories`}
-          </h1>
-        )}
-      </div>
-
-      {!fetchedData.length && (
+}: ListRepositoriesProps): JSX.Element => {
+  if (!fetchedData.length) {
+    return (
+      <div className="mx-auto max-w-7xl px-4 mt-10">
         <div className="flex flex-col gap-y-5 overflow-hidden mb-12">
-          {Array.from(Array(5).keys()).map(item => (
+          {Array.from(Array(10).keys()).map(item => (
             <div
               key={item}
               className="p-4 border rounded-2xl bg-white w-full space-y-1 relative"
@@ -42,27 +32,43 @@ const ListRepositories = ({
             </div>
           ))}
         </div>
-      )}
-
-      {fetchedData.map((item, i) => (
-        <RepoList
-          key={`${item.full_name}_${i}`}
-          data={item}
-        />
-      ))}
-    </div>
-
-    {fetchedData.length > 0 && activeLink !== "myVotes" && limit <= 100 && (
-      <div className="flex justify-center">
-        <button
-          className="bg-white text-gray-700 mt-4 mb-4 text-base border-gray-400 border font-normal py-1 px-4 rounded"
-          onClick={() => handleLoadingMore()}
-        >
-          Load More
-        </button>
       </div>
-    )}
-  </div>
-);
+    );
+  }
+
+  return (
+    <div className="mx-auto max-w-7xl px-4 mt-10">
+      <div className="flex flex-col gap-y-5">
+        <div className="flex items-center gap-x-2.5">
+          <BsFillCalendar2Fill className="w-8 h-8 text-white" />
+
+          {activeLink && (
+            <h1 className="text-2xl text-white font-semibold">
+              {`${camelCaseToTitleCase(activeLink)} Repositories`}
+            </h1>
+          )}
+        </div>
+
+        {fetchedData.map((item, i) => (
+          <RepoList
+            key={`${item.full_name}_${i}`}
+            data={item}
+          />
+        ))}
+      </div>
+
+      {fetchedData.length > 0 && activeLink !== "myVotes" && limit <= 100 && (
+        <div className="flex justify-center">
+          <button
+            className="bg-white text-gray-700 mt-4 mb-4 text-base border-gray-400 border font-normal py-1 px-4 rounded"
+            onClick={() => handleLoadingMore()}
+          >
+            Load More
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ListRepositories;

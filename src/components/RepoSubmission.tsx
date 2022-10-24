5e9974893e4ac5f021c27eb60f64bc4afcc594b8
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-10-24 19:41:53 -0400

    submit not working..

--- src/components/RepoSubmission.tsx
+++ src/components/RepoSubmission.tsx
@@ -4,6 +4,7 @@ import { sendMessage } from "../lib/discord";
 import isValidRepoUrl from "../lib/validateUrl";
 import { ToastTrigger } from "../lib/reactHotToast";
 import useSupabaseAuth from "../hooks/useSupabaseAuth";
+import { supabase } from "../lib/supabase";
 
 export declare interface RepoSubmissionProps {
   isFormOpen: boolean;
@@ -12,6 +13,7 @@ export declare interface RepoSubmissionProps {
 
 const RepoSubmission = ({ isFormOpen, handleFormOpen }: RepoSubmissionProps): JSX.Element => {
   const { user } = useSupabaseAuth();
+  const currentUser = supabase.auth.session();
   const [isSubmissionInProcess, setIsSubmissionInProcess] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const [input, setInput] = useState("");
@@ -37,10 +39,20 @@ const RepoSubmission = ({ isFormOpen, handleFormOpen }: RepoSubmissionProps): JS
         return handleFormOpen(false);
       }
 
+      if (currentUser?.access_token) submitRepo(sanitizedUrl, currentUser.access_token);
       setSubmitted(true);
       sendMessage(userName, sanitizedUrl);
       return ToastTrigger({ message: "Data Submitted", type: "success" });
     }, 500);
+  };
+
+  const submitRepo = async (sanitizedUrl: string, userToken: string) => {
+    await fetch(`${import.meta.env.VITE_API_URL}/repos/${sanitizedUrl}`, {
+      method: "PUT",
+      headers: { accept: "application/json", Authorization: `Bearer ${userToken}` },
+    })
+      .then((res) => console.log("response: ", res))
+      .catch((err) => console.log("error: ", err));
   };
 
   const submitButtonHandler = (): void => {
@@ -55,7 +67,7 @@ const RepoSubmission = ({ isFormOpen, handleFormOpen }: RepoSubmissionProps): JS
   };
 
   // listening outside focus
-  function handleClickOutsideRepoSubmission (): void {
+  function handleClickOutsideRepoSubmission(): void {
     if (isSubmissionInProcess) {
       return;
     }
@@ -65,10 +77,7 @@ const RepoSubmission = ({ isFormOpen, handleFormOpen }: RepoSubmissionProps): JS
   }
 
   return (
-    <div
-      ref={submissionRef}
-      className="fixed top-14 right-40 flex items-end flex-col gap-2.5 submission-form z-10"
-    >
+    <div ref={submissionRef} className="fixed top-14 right-40 flex items-end flex-col gap-2.5 submission-form z-10">
       {isFormOpen}
 
       {isFormOpen && !isSubmissionInProcess && !submitted && (
@@ -81,7 +90,7 @@ const RepoSubmission = ({ isFormOpen, handleFormOpen }: RepoSubmissionProps): JS
             className="bg-gray-200 py-1 w-full px-2.5 rounded-md outline-yellow-300 text-gray-500 text-xs  "
             placeholder="https://github.com/open-sauced/hot"
             type="text"
-            onChange={e => setInput(e.target.value)}
+            onChange={(e) => setInput(e.target.value)}
           />
 
           <button

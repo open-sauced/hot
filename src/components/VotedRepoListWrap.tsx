82c09d199b0d2be5a552325d5ed486ca83e04b26
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-11-03 23:38:43 +0200

    revert week categorization in myVotes

--- src/components/VotedRepoListWrap.tsx
+++ src/components/VotedRepoListWrap.tsx
@@ -5,7 +5,6 @@ import HotRepositories from "./HotRepositories";
 import ListRepositories from "./ListRepositories";
 import SecondaryNav from "./SecondaryNav";
 import { useVotedRepositoriesList } from "../hooks/useVotedRepositoriesList";
-import { useEffect, useState } from "react";
 
 export enum RepoOrderByEnum {
   popular = "stars",
@@ -39,33 +38,6 @@ const VotedRepoListWrap = (): JSX.Element => {
   const limit = parseLimitValue(searchParams.get("limit"));
 
   const { data, isLoading } = useVotedRepositoriesList(RepoOrderByEnum[activeLink], limit);
-  const [thisWeek, setThisWeek] = useState<DbRepo[] | null>(null);
-  const [lastWeek, setLastWeek] = useState<DbRepo[] | null>(null);
-  const [older, setOlder] = useState<DbRepo[] | null>(null);
-
-  useEffect( () => {
-    const lastSunday = (new Date);
-
-    lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay());
-    lastSunday.setHours(0, 0, 0, 0);
-
-    if (!isLoading) {
-      const thisWeekData = data.filter( repo => repo.created_at && new Date(repo.created_at) > lastSunday);
-
-      thisWeekData.sort( (a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
-      setThisWeek(thisWeekData);
-
-      const lastWeekData = data.filter( repo => repo.created_at && new Date(repo.created_at) < lastSunday && new Date(repo.created_at) > new Date(lastSunday.getTime() - 7 * 24 * 60 * 60 * 1000));
-
-      lastWeekData.sort( (a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
-      setLastWeek(lastWeekData);
-
-      const olderData = data.filter( repo => repo.created_at && new Date(repo.created_at) < new Date(lastSunday.getTime() - 7 * 24 * 60 * 60 * 1000));
-
-      olderData.sort( (a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
-      setOlder(olderData);
-    }
-  }, [data]);
 
   const handleLoadingMore = () => {
     setSearchParams({ limit: String(limit + 10) });
@@ -81,36 +53,13 @@ const VotedRepoListWrap = (): JSX.Element => {
       <HotRepositories />
 
       {!isLoading &&
-        (
-          <>
-            {thisWeek && thisWeek.length > 0 &&
-              <ListRepositories
-                activeLink={activeLink}
-                fetchedData={thisWeek}
-                handleLoadingMore={handleLoadingMore}
-                limit={limit}
-                title="this week"
-              />}
-
-            {lastWeek && lastWeek.length > 0 &&
-              <ListRepositories
-                activeLink={activeLink}
-                fetchedData={lastWeek}
-                handleLoadingMore={handleLoadingMore}
-                limit={limit}
-                title="last week"
-              />}
-
-            {older && older.length > 0 &&
-              <ListRepositories
-                activeLink={activeLink}
-                fetchedData={older}
-                handleLoadingMore={handleLoadingMore}
-                limit={limit}
-                title="Older"
-              />}
-          </>
-        )}
+        <ListRepositories
+          activeLink={activeLink}
+          fetchedData={data}
+          handleLoadingMore={handleLoadingMore}
+          limit={limit}
+          title={`My Votes Repositories`}
+        />}
     </div>
   );
 };

7ebf725a660dea50df4931fc51dbd3e660bc2411
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2022-11-29 23:20:31 +0200

    feat: placeholder info for my votes if no votes

--- src/components/VotedRepoListWrap.tsx
+++ src/components/VotedRepoListWrap.tsx
@@ -3,6 +3,7 @@ import locationsHash from "../lib/locationsHash";
 import HotRepositories from "./HotRepositories";
 import ListRepositories from "./ListRepositories";
 import { useVotedRepositoriesList } from "../hooks/useVotedRepositoriesList";
+import { BsFillCalendar2Fill } from "react-icons/bs";
 
 export enum RepoOrderByEnum {
   popular = "stars",
@@ -40,14 +41,41 @@ const VotedRepoListWrap = (): JSX.Element => {
     setSearchParams({ limit: String(limit + 10) });
   };
 
+  if(!isLoading && !data?.length) {
+    return (
+      <div className="mx-auto max-w-7xl px-4 pb-1 mt-10">
+        <div className="flex flex-col gap-y-5 overflow-hidden mb-12">
+          <div className="flex items-center gap-x-2.5">
+            <BsFillCalendar2Fill className="w-8 h-8 text-white" />
+              <h1 className="text-2xl text-white font-semibold">
+                My Votes Repositories
+              </h1>
+          </div>
+          <div
+            className="p-4 border rounded-2xl bg-white w-full space-y-1 relative"
+          >
+            <h1 className="text-2xl text-center">You didn't vote on any Repositories. Your votes will appear here.</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <ListRepositories
-      activeLink={activeLink}
-      fetchedData={data}
-      handleLoadingMore={handleLoadingMore}
-      limit={limit}
-      title="My Votes Repositories"
-    />
+    <>
+      {
+        !isLoading && !data.length ?
+        <div></div>
+          :
+          <ListRepositories
+            activeLink={activeLink}
+            fetchedData={data}
+            handleLoadingMore={handleLoadingMore}
+            limit={limit}
+            title="My Votes Repositories"
+          />
+      }
+    </>
   );
 };
 

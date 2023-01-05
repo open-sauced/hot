7eaedcaa05ad44c5a6bf8a86a1de662133b2e730
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-01-05 14:49:23 +0100

    refactor: update to dynamic date string

--- src/lib/getDateFromNow.test.ts
+++ src/lib/getDateFromNow.test.ts
@@ -3,7 +3,10 @@ import { describe, expect, test } from "vitest";
 
 describe("Test: getDateFromNow() without suffix", () => {
   test("should return 20 years", () => {
-    const dateString = "1999-01-01";
+    const date = (new Date);
+
+    date.setFullYear(date.getFullYear() - 24);
+    const dateString = date.toISOString().substring(0, 10);
     const result = getDateFromNow(dateString, true);
 
     expect(result).toBe("24 years");
@@ -12,9 +15,13 @@ describe("Test: getDateFromNow() without suffix", () => {
 
 describe("Test: getDateFromNow() with suffix", () => {
   test("should return 20 years", () => {
-    const dateString = "1999-01-01";
+    const date = (new Date);
+
+    date.setFullYear(date.getFullYear() - 20);
+    const dateString = date.toISOString().substring(0, 10);
+
     const result = getDateFromNow(dateString);
 
-    expect(result).toBe("24 years ago");
+    expect(result).toBe("20 years ago");
   });
 });

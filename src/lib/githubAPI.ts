8603ac1caacb48aae203ae57dc2be3c767d3beaf
Josh Goldberg <git@joshuakgoldberg.com>
2023-01-05 17:39:03 -0500

    chore: used ts-prune to find unused code

--- src/lib/githubAPI.ts
+++ src/lib/githubAPI.ts
@@ -1,20 +1,4 @@
-export interface ReposCountFetchObject {
-  total_count: number;
-}
-
-export async function getOpensaucedGoalsReposCount (): Promise<number> {
-  const res = await fetch(`https://api.github.com/search/repositories?q=open-sauced-goals&order=asc&per_page=100`);
-
-  const data = (await res.json()) as ReposCountFetchObject;
-
-  if (res.ok) {
-    return data.total_count;
-  }
-  console.error(data);
-  return 0;
-}
-
-export async function getUserStarredHotRepo (githubToken: string): Promise<boolean> {
+export async function getUserStarredHotRepo(githubToken: string): Promise<boolean> {
   const result = await fetch("https://api.github.com/user/starred/open-sauced/hot", {
     headers: {
       Accept: "application/vnd.github+json",

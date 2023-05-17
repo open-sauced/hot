c6da7523180ed6789bd9ae2189e152cc7faafa0a
TED Vortex (Teodor-Eugen Duțulescu) <237133+0-vortex@users.noreply.github.com>
2023-05-17 13:49:29 +0200

    fix: correct squoosh leveled dependencies (#468)
    * fix: correct leveled dependencies
    
    * fix: force netlify node version

--- .squoosh.mjs
+++ .squoosh.mjs
@@ -1,46 +1,66 @@
-import { dirname } from "path";
-import {execa} from "execa";
+import { mkdtemp } from 'node:fs/promises';
+import { join } from 'node:path';
+import { tmpdir } from 'node:os';
+import { writeFileSync } from "node:fs";
 import glob from "glob";
 import pLimit from "p-limit";
+import sharp from "sharp";
 
 const limit = pLimit(3);
 const [globs] = process.argv.slice(2);
-const algo = {
-  'jpg': '--mozjpeg',
-  'jpeg': '--mozjpeg',
-  'png': '--oxipng',
-  'webp': '--webp',
-  'avif': '--avif',
-  'jxl': '--jxl',
-  'wp2': '--wp2',
-};
 const promises = [];
 const errors = [];
 
 const imgs = glob.sync(globs, {nonull: false});
 
-imgs && imgs.map(async (img) => promises.push(
-  limit(async () => {
-    try {
-      const dir = dirname(img);
-      const ext = /\.([^\.]+)$/ig.exec(img)[1];
+if (imgs.length !== 0) {
+  const dir = await mkdtemp(join(tmpdir(), 'optimise-'));
 
-      await execa("squoosh-cli", [
-        algo[ext],
-        "auto",
-        "--output-dir",
-        dir,
-        img
-      ], {
-        stderr: process.stdout,
-        stdout: process.stdout
-      });
-    } catch (e) {
-      console.log(e);
-      // errors.push(e.message || e);
-    }
-  })
-));
+  imgs.map(async (img) => promises.push(
+    limit(async () => {
+      try {
+        const ext = /\.([^\.]+)$/ig.exec(img)[1];
+        const outputFile = join(dir, img);
+
+        // Convert and optimize images
+        let image = sharp(img);
+
+        switch (ext) {
+          case 'jpg':
+            image.jpeg({ mozjpeg: true });
+            break;
+          case 'jpeg':
+            image.jpeg({ mozjpeg: true });
+            break;
+          case 'png':
+            image.png({ oxipng: true });
+            break;
+          case 'webp':
+            image.webp();
+            break;
+          case 'avif':
+            image.avif();
+            break;
+          case 'jxl':
+            image.jxl();
+            break;
+          case 'wp2':
+            image.wp2();
+            break;
+          default:
+            throw new Error(`Unsupported file extension: ${ext}`);
+        }
+
+        writeFileSync(img, await image.toBuffer());
+
+        // await image.toFile(outputFile);
+      } catch (e) {
+        console.log(e);
+        errors.push(e.message || e);
+      }
+    })
+  ));
+}
 
 await Promise.all(promises);
 

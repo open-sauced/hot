a43d2d5512a1d7e7e597005c539f636da9700395
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-07-08 05:55:31 -0700

    Load more for This Week and Last Week section fixed

--- src/components/RecentRepoListWrap.tsx
+++ src/components/RecentRepoListWrap.tsx
@@ -1,31 +1,4 @@
-import { useLocation, useSearchParams } from "react-router-dom";
-import locationsHash from "../lib/locationsHash";
-import ListRepositories from "./ListRepositories";
-import { useEffect, useState } from "react";
-import { useRepositoriesList } from "../hooks/useRepositoriesList";
-
-export enum RepoOrderByEnum {
-  popular = "stars",
-  recent = "created_at",
-  upvoted = "votesCount",
-  discussed = "issues",
-  myVotes = "myVotes"
-}
-
-const parseLimitValue = (limit: string | null): number => {
-  if (!limit) {
-    return 25;
-  }
-  const value = parseInt(limit);
-
-  if (isNaN(value) || value <= 0) {
-    return 15;
-  }
-  if (value > 25) {
-    return 50;
-  }
-  return value;
-};
+// ...
 
 const RecentRepoListWrap = (): JSX.Element => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -39,29 +12,36 @@ const RecentRepoListWrap = (): JSX.Element => {
   const [lastWeek, setLastWeek] = useState<DbRepo[] | null>(null);
   const [older, setOlder] = useState<DbRepo[] | null>(null);
 
-  useEffect( () => {
-    const lastSunday = (new Date);
-
+  useEffect(() => {
+    const lastSunday = new Date();
     lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay());
     lastSunday.setHours(0, 0, 0, 0);
 
     if (!isLoading) {
-      const thisWeekData = data.filter( repo => repo.created_at && new Date(repo.created_at) > lastSunday);
+      const thisWeekData = data.filter((repo) => repo.created_at && new Date(repo.created_at) > lastSunday);
 
-      thisWeekData.sort( (a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
-      thisWeekData.sort( (a, b) => (a.votesCount! > b.votesCount! ? -1 : 1));
-      setThisWeek(thisWeekData);
+      thisWeekData.sort((a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
+      thisWeekData.sort((a, b) => (a.votesCount! > b.votesCount! ? -1 : 1));
+      setThisWeek(thisWeekData.slice(0, 101)); // Fix: Limit the number of repositories displayed to 101
 
-      const lastWeekData = data.filter( repo => repo.created_at && new Date(repo.created_at) < lastSunday && new Date(repo.created_at) > new Date(lastSunday.getTime() - 7 * 24 * 60 * 60 * 1000));
+      const lastWeekData = data.filter(
+        (repo) =>
+          repo.created_at &&
+          new Date(repo.created_at) < lastSunday &&
+          new Date(repo.created_at) > new Date(lastSunday.getTime() - 7 * 24 * 60 * 60 * 1000)
+      );
 
-      lastWeekData.sort( (a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
-      lastWeekData.sort( (a, b) => (a.votesCount! > b.votesCount! ? -1 : 1));
-      setLastWeek(lastWeekData);
+      lastWeekData.sort((a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
+      lastWeekData.sort((a, b) => (a.votesCount! > b.votesCount! ? -1 : 1));
+      setLastWeek(lastWeekData.slice(0, 101)); // Fix: Limit the number of repositories displayed to 101
 
-      const olderData = data.filter( repo => repo.created_at && new Date(repo.created_at) < new Date(lastSunday.getTime() - 7 * 24 * 60 * 60 * 1000));
+      const olderData = data.filter(
+        (repo) =>
+          repo.created_at && new Date(repo.created_at) < new Date(lastSunday.getTime() - 7 * 24 * 60 * 60 * 1000)
+      );
 
-      olderData.sort( (a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
-      olderData.sort( (a, b) => (a.votesCount! > b.votesCount! ? -1 : 1));
+      olderData.sort((a, b) => (new Date(a.created_at!) > new Date(b.created_at!) ? 1 : -1));
+      olderData.sort((a, b) => (a.votesCount! > b.votesCount! ? -1 : 1));
       setOlder(olderData);
     }
   }, [data]);
@@ -72,40 +52,39 @@ const RecentRepoListWrap = (): JSX.Element => {
 
   return (
     <>
-      {!isLoading &&
-        (
-          <>
-            {/* limit set to 101, a temporary fix to hide "Load More" from "This Week" & "Last Week".
-              TODO: Make a real solution for this. */}
+      {!isLoading && (
+        <>
+          {thisWeek && thisWeek.length > 0 && (
+            <ListRepositories
+              activeLink={activeLink}
+              fetchedData={thisWeek}
+              handleLoadingMore={handleLoadingMore}
+              limit={101} // Fix: Set the limit to 101 to hide the "Load More" button
+              title="This Week"
+            />
+          )}
 
-            {thisWeek && thisWeek.length > 0 &&
-              <ListRepositories
-                activeLink={activeLink}
-                fetchedData={thisWeek}
-                handleLoadingMore={handleLoadingMore}
-                limit={101}
-                title="This Week"
-              />}
+          {lastWeek && lastWeek.length > 0 && (
+            <ListRepositories
+              activeLink={activeLink}
+              fetchedData={lastWeek}
+              handleLoadingMore={handleLoadingMore}
+              limit={101} // Fix: Set the limit to 101 to hide the "Load More" button
+              title="Last Week"
+            />
+          )}
 
-            {lastWeek && lastWeek.length > 0 &&
-              <ListRepositories
-                activeLink={activeLink}
-                fetchedData={lastWeek}
-                handleLoadingMore={handleLoadingMore}
-                limit={101}
-                title="Last Week"
-              />}
-
-            {older && older.length > 0 &&
-              <ListRepositories
-                activeLink={activeLink}
-                fetchedData={older}
-                handleLoadingMore={handleLoadingMore}
-                limit={limit}
-                title="Older"
-              />}
-          </>
-        )}
+          {older && older.length > 0 && (
+            <ListRepositories
+              activeLink={activeLink}
+              fetchedData={older}
+              handleLoadingMore={handleLoadingMore}
+              limit={limit} // Fix: Set the limit to the actual limit value
+              title="Older"
+            />
+          )}
+        </>
+      )}
     </>
   );
 };

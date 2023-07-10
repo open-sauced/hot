cad88afe0a90339fbd9734473e51c7b1d7c98d46
ali navidi <ali_navidi24@yahoo.com>
2023-07-10 17:58:21 +0330

    ESLint bugs fixed

--- src/components/ListRepositories.tsx
+++ src/components/ListRepositories.tsx
@@ -1,5 +1,4 @@
 import { BsFillCalendar2Fill } from "react-icons/bs";
-import Skeleton from "react-loading-skeleton";
 import RepoList from "./RepoList";
 
 export declare interface ListRepositoriesProps {
@@ -16,33 +15,37 @@ const ListRepositories = ({
   handleLoadingMore,
   fetchedData,
   title,
-}: ListRepositoriesProps): JSX.Element => {
-  return (
-    <div className="mx-auto max-w-7xl px-4 pb-1 mt-10">
-      <div className="flex flex-col gap-y-5 mb-12">
-        <div className="flex items-center gap-x-2.5">
-          <BsFillCalendar2Fill className="w-8 h-8 text-white" />
+}: ListRepositoriesProps): JSX.Element => (
+  <div className="mx-auto max-w-7xl px-4 pb-1 mt-10">
+    <div className="flex flex-col gap-y-5 mb-12">
+      <div className="flex items-center gap-x-2.5">
+        <BsFillCalendar2Fill className="w-8 h-8 text-white" />
 
-          {activeLink && <h1 className="text-2xl text-white font-semibold">{title}</h1>}
-        </div>
-
-        {fetchedData.map((item, i) => (
-          <RepoList key={`${item.full_name}_${i}`} data={item} />
-        ))}
+        {activeLink &&
+          <h1 className="text-2xl text-white font-semibold">
+            {title}
+          </h1>}
       </div>
 
-      {fetchedData.length > 0 && activeLink !== "myVotes" && limit <= 100 && (
-        <div className="flex justify-center">
-          <button
-            className="bg-white text-gray-700 mt-4 mb-4 text-base border-gray-400 border font-normal py-1 px-4 rounded"
-            onClick={() => handleLoadingMore()}
-          >
-            Load More
-          </button>
-        </div>
-      )}
+      {fetchedData.map((item, i) => (
+        <RepoList
+          key={`${item.full_name}_${i}`}
+          data={item}
+        />
+      ))}
     </div>
-  );
-};
+
+    {fetchedData.length > 0 && activeLink !== "myVotes" && limit <= 100 && (
+      <div className="flex justify-center">
+        <button
+          className="bg-white text-gray-700 mt-4 mb-4 text-base border-gray-400 border font-normal py-1 px-4 rounded"
+          onClick={() => handleLoadingMore()}
+        >
+          Load More
+        </button>
+      </div>
+    )}
+  </div>
+);
 
 export default ListRepositories;

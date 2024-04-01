fcf1183574ab7e6b2245677065d2c82e5f8d1aa4
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2024-04-01 15:48:49 -0700

    swapping github links for opensauced ones.

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -2,10 +2,10 @@ const getAvatarLink = (username: string | null) =>
   `https://github.com/${username ?? "github"}.png?size=460`;
 
 const getProfileLink = (username: string | null) =>
-  `https://github.com/${username ?? ""}`;
+  `https://app.opensauced.pizza/user/${username ?? ""}`;
 
 const getRepoLink = (repoName: string | null) =>
-  `https://app.opensauced.pizza/${repoName ?? ""}`;
+  `https://app.opensauced.pizza/s/${repoName ?? ""}`;
 
 const getRepoIssuesLink = (repoName: string | null) =>
   `https://github.com/${repoName && `${repoName}/issues` || ""}`;

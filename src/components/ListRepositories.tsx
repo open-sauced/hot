b7f9580bac24d0f6d57f7739b77db1574ed29184
furqan-abid <furqanabid10@gmail.com>
2024-04-14 21:42:21 +0500

    fix/remove search bar from hero section

--- src/components/ListRepositories.tsx
+++ src/components/ListRepositories.tsx
@@ -13,9 +13,15 @@ const ListRepositories = ({ activeLink, fetchedData, title }: ListRepositoriesPr
     return (
       <div className="mx-auto max-w-7xl px-4 mt-10">
         <div className="flex flex-col gap-y-5 overflow-hidden mb-12">
-          {Array.from(Array(25).keys()).map((item) => (
-            <div key={item} className="p-4 border rounded-2xl bg-white w-full space-y-1 relative">
-              <Skeleton enableAnimation count={4} />
+          {Array.from(Array(25).keys()).map(item => (
+            <div
+              key={item}
+              className="p-4 border rounded-2xl bg-white w-full space-y-1 relative"
+            >
+              <Skeleton
+                enableAnimation
+                count={4}
+              />
             </div>
           ))}
         </div>
@@ -29,11 +35,16 @@ const ListRepositories = ({ activeLink, fetchedData, title }: ListRepositoriesPr
         <div className="flex items-center gap-x-2.5">
           <BsFillCalendar2Fill className="w-8 h-8 text-white" />
 
-          {activeLink && <h1 className="text-2xl text-white font-semibold">{title}</h1>}
+          {activeLink && <h1 className="text-2xl text-white font-semibold">
+            {title}
+          </h1>}
         </div>
 
         {fetchedData.map((item, i) => (
-          <RepoList key={`${item.repo_name}_${i}`} data={item} />
+          <RepoList
+            key={`${item.repo_name}_${i}`}
+            data={item}
+          />
         ))}
       </div>
     </div>
